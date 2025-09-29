982fc51607920e12d6e376f74844726868e25f56
CHIDANANDA K P <chidanandakp31@gmail.com>
2025-09-29 22:19:27 +0530

    Add files via upload

---
+++ JavaBasics.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class JavaBasics {
+    public static void main(String[] args) {
+        Scanner nc =new Scanner(System.in);
+        String raj=nc.next();
+        System.out.println(raj);
+        
+    }
+}
+
